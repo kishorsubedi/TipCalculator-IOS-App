1846b253336f2a7c9ca649a068321c197b88cf2d
Nimesh Baral <nimesh@nimeshs-air.fios-router.home>
2018-01-23 21:34:33 -0500

    Initial Commit

---
+++ tipsy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipsy
+//
+//  Created by Nimesh Baral on 1/23/18.
+//  Copyright © 2018 Nimesh Baral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
